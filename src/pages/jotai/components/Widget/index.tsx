cb38806a66428a9a7584312973c9e1925a0ba5ab
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2022-06-01 19:10:57 +0000

    a bit of context

--- src/pages/jotai/components/Widget/index.tsx
+++ src/pages/jotai/components/Widget/index.tsx
@@ -1,4 +1,5 @@
 import { Provider } from "jotai";
+import { createContext } from "react";
 import { GetContentQuery } from "../../../../generated/graphql";
 import { createInitialValues } from "../../../../lib/jotai/helpers/createInitialValues";
 import useWidgetQuantity, { quantityAtom } from "../../hooks/useWidgetQuantity";
@@ -7,38 +8,52 @@ interface WidgetProps {
     widget: GetContentQuery['widgets'][0];
 }
 
-function Inner({ widget }: WidgetProps) {
+const jotaiContext = createContext<WidgetProps | undefined>(undefined);
+
+function Inner() {
     const { quantity, increase, decrease } = useWidgetQuantity();
     return (
-        <article>
-            <p>{widget.name} &times; {quantity || 0}</p>
-            <div className="grid">
-                <button
-                    className="outline"
-                    onClick={decrease}
-                >
-                    -
-                </button>
-                <button
-                    className="outline"
-                    onClick={increase}
-                >
-                    +
-                </button>
-            </div>
-        </article>
+        <jotaiContext.Consumer>
+            {(props) => {
+                if (!props) {
+                    return;
+                }
+                const { widget } = props;
+                return (
+                    <article>
+                        <p>{widget.name} &times; {quantity || 0}</p>
+                        <div className="grid">
+                            <button
+                                className="outline"
+                                onClick={decrease}
+                            >
+                                -
+                            </button>
+                            <button
+                                className="outline"
+                                onClick={increase}
+                            >
+                                +
+                            </button>
+                        </div>
+                    </article>
+                );
+            }}
+        </jotaiContext.Consumer>
     )
 }
 
-export default function Widget({ widget }: WidgetProps) {
+export default function Widget(props: WidgetProps) {
     const { get, set } = createInitialValues();
     set(quantityAtom, 0);
     const initialValues = get();
     return (
-        <Provider
-            initialValues={initialValues}
-        >
-            <Inner widget={widget} />
-        </Provider>
+        <jotaiContext.Provider value={props}>
+            <Provider
+                initialValues={initialValues}
+            >
+                <Inner />
+            </Provider>
+        </jotaiContext.Provider>
     )
 }
